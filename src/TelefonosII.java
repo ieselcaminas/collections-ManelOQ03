b2ae32587452bda31a1e92d74e9b2b0d64e00b58
manortque <manortque@alu.edu.gva.es>
2024-11-28 13:49:06 +0100

    Versión final

---
+++ src/TelefonosII.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TelefonosII {
+    public static void main(String[] args) {
+        Map<String, ArrayList<Integer>> guia = new HashMap<String, ArrayList<Integer>>();
+        ArrayList<String> telefonosPepe = new ArrayList();
+    }
+}
