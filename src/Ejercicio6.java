06a6b9852f82c36d354f6e3382b9bcd7e37c6470
manortque <manortque@alu.edu.gva.es>
2024-12-12 13:55:13 +0100

    Versión final

---
+++ src/Ejercicio6.java
@@ -0,0 +1,25 @@
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        String numero = "4678";
+        if(esHyperpar(numero)){
+            System.out.println("Es Hyperpar");
+        } else {
+            System.out.println("No es Hyperpar");
+        }
+    }
+    public static boolean esHyperpar(String numero) {
+        char digito;
+        int digito1;
+        for (int i = 0; i < numero.length(); i++) {
+            digito = numero.charAt(i);
+            digito1 = digito;
+            if (digito %2 == '0') {
+                return true;
+            } else if (digito %2 == '1') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
--- src/Parentesis.java
+++ src/Parentesis.java
@@ -2,9 +2,9 @@ import java.util.Stack;
 
 public class Parentesis {
     public static void main(String[] args) {
-        System.out.println(esBalanceado("{([()])}"));
+        //System.out.println(esBalanceado("{([()])}"));
     }
-    public static boolean esBalanceado(String cadena){
+    /* public static boolean esBalanceado(String cadena){
         boolean balanceado = false;
         char character;
         char ultimo;
@@ -12,9 +12,24 @@ public class Parentesis {
         for (int i = 0; i < cadena.length(); i++) {
             //Si son apertura los pongo en la pila
             character = cadena.charAt(i);
+            if (character == '(' || character == '{' || character == '[') {
+                pila.push(character);
+            } else if (character == ')' || character == '}' || character == ']') {
+                if (pila.isEmpty()) {
+                    return false;
+                }
+                ultimo = pila.pop();
+                //Saco de la pila y compruebo si es '['. Si no es, acabo con false
+                if (ultimo != '('){
+                    return false;
+                }
+            } //else if () {
+                
+            }
+
         }
 
 
         return true;
-    }
+    }*/
 }
